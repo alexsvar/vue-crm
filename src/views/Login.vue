b331e8e02c3fac7c458646bd480c948d0ada8b18
Alexander Morozov <alexsvar@icloud.com>
2020-10-01 14:26:07 +0300

    added login form's validation

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,24 +1,44 @@
 <template>
-  <form class="card auth-card">
+  <form class="card auth-card" @submit.prevent="submitHandler">
     <div class="card-content">
       <span class="card-title">Home CRM</span>
       <div class="input-field">
         <input
           id="email"
           type="text"
-          class="validate"
+          v-model.trim="email"
+          :class="{invalid:
+            ($v.email.$dirty && !$v.email.required) || ($v.email.$dirty && !$v.email.email)}"
         >
         <label for="email">Email</label>
-        <small class="helper-text invalid">Email</small>
+        <small
+          v-if="$v.email.$dirty && !$v.email.required"
+          class="helper-text invalid"
+        >
+          The email field should not be empty
+        </small>
+        <small
+          v-if="$v.email.$dirty && !$v.email.email"
+          class="helper-text invalid"
+        >
+          Enter the correct email address
+        </small>
       </div>
       <div class="input-field">
         <input
           id="password"
           type="password"
-          class="validate"
+          v-model.trim="password"
+          :class="{invalid:
+            ($v.password.$dirty && !$v.password.required)}"
         >
         <label for="password">Password</label>
-        <small class="helper-text invalid">Password</small>
+        <small
+          v-if="$v.password.$dirty && !$v.password.required"
+          class="helper-text invalid"
+        >
+          Enter the correct password
+        </small>
       </div>
     </div>
     <div class="card-action">
@@ -34,15 +54,41 @@
 
       <p class="center">
         Don't have an account?
-        <a href="/">Register</a>
+        <router-link to="/register">Register</router-link>
       </p>
     </div>
   </form>
 </template>
 
 <script>
+import {email, required, minLength} from 'vuelidate/lib/validators'
+
 export default {
-  name: 'Login'
+  name: 'Login',
+  data: () => ({
+    email: '',
+    password: ''
+  }),
+  validations: {
+    email: {email, required},
+    password: {required}
+  },
+  methods: {
+    submitHandler() {
+      if (this.$v.$invalid) {
+        this.$v.$touch()
+        return
+      }
+
+      const formData = {
+        email: this.email,
+        password: this.password
+      }
+      console.log(formData)
+
+      this.$router.push('/')
+    }
+  }
 }
 </script>
 
