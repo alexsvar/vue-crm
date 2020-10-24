915fa79072d63854e02329e486f998efaaf0ebac
Alexander Morozov <alexsvar@icloud.com>
2020-10-24 17:43:08 +0300

    added data output to the detail page

--- src/store/note.js
+++ src/store/note.js
@@ -26,6 +26,21 @@ export default {
         commit('setError', e)
         throw e
       }
+    },
+    async fetchNoteById({dispatch, commit}, id) {
+      try {
+        const uid = await dispatch('getUid')
+        const note = (
+          await firebase.database()
+            .ref(`/users/${uid}/notes`)
+            .child(id)
+            .once('value')
+        ).val() || {}
+        return {...note, id}
+      } catch (e) {
+        commit('setError', e)
+        throw e
+      }
     }
   }
 }
--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -1,30 +1,50 @@
 <template>
   <div>
-    <div class="breadcrumb-wrap">
-      <a href="/history" class="breadcrumb">History</a>
-      <a class="breadcrumb">
-        Outcome
-      </a>
-    </div>
-    <div class="row">
-      <div class="col s12 m6">
-        <div class="card red">
-          <div class="card-content white-text">
-            <p>Description:</p>
-            <p>Amount:</p>
-            <p>Category:</p>
+    <Loader v-if="loading"/>
+    <div v-else-if="note">
+      <div class="breadcrumb-wrap">
+        <router-link to="/history" class="breadcrumb">History</router-link>
+        <a class="breadcrumb" @click.prevent>
+          {{note.type === 'income' ? 'Income' : 'Outcome'}}
+        </a>
+      </div>
+      <div class="row">
+        <div class="col s12 m6">
+          <div class="card" :class="note.type === 'income' ? 'green' : 'red'">
+            <div class="card-content white-text">
+              <p>Description: {{note.description}}</p>
+              <p>Amount: {{note.amount | currency}}</p>
+              <p>Category: {{note.categoryName}}</p>
 
-            <small>12.12.12</small>
+              <small>{{note.date | date('datetime')}}</small>
+            </div>
           </div>
         </div>
       </div>
     </div>
+    <p class="center" v-else>This note is not found</p>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Detail'
+  name: 'Detail',
+  data: () => ({
+    note: null,
+    loading: true
+  }),
+  async mounted() {
+    const id = this.$route.params.id
+    const note = await this.$store.dispatch('fetchNoteById', id)
+    const category = await this.$store.dispatch('fetchCategoryById', note.categoryId)
+
+    this.note = {
+      ...note,
+      categoryName: category.title
+    }
+
+    this.loading = false
+  }
 }
 </script>
 
